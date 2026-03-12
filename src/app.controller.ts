a44bbe60e0a74fd56582e58b62de3ea7991c5a12
Hoang Kim Long <longhk.dev@gmail.com>
2026-03-12 21:50:02 +0700

    mod: update health check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -18,7 +18,7 @@ export class AppController {
 
   @Get('health')
   @Public()
-  @ApiOperation({ summary: 'Health Check Backend Test' })
+  @ApiOperation({ summary: 'Health Check Backend Test Health' })
   @ApiResponse({ status: 200, description: 'Returns health status' })
   healthCheck(): { status: string; timestamp: string } {
     return {
